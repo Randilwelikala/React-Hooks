639e292a5da14e5891695437edd0829273bb60e6
randilwelikala <randilgimantha646@gmail.com>
2025-03-31 00:21:16 +0530

    learn useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [setx,xhandler] = useState(0);
 
   var x = 0;
 
   const counter = () =>{
     // console.log('button Click');
     x=x+1;
-    console.log(`new value of x- ${x}`);
+    xhandler(setx +1);
+    console.log(`new value of x = ${x}`);
   }; 
+  const reduser = () =>{
+    xhandler(setx-1);
+  };
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {x}
+          Value of X = {x}
         </p>
-        <button onClick = {counter}>Click Me</button>
+
+        <p>
+          Value of Count = {setx};
+        </p>
+
+        <button onClick = {counter}>Click Me +</button>
+        <button onClick={reduser}> Click Me -</button>
         <a
           className="App-link"
           href="https://reactjs.org"
